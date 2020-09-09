065fcb5b5665ecb627d6176993fc5458d5dbff81
Abhiram13 <abhiram131869@gmail.com>
2020-09-09 20:48:26 +0530

    Inital Commit

---
+++ COVID/Object.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BlazorApp.Data
+{
+   public class Mordor
+   {
+      public string id { get; set; }
+      public string itemName { get; set; }
+      public string description { get; set; }
+      public string category { get; set; }
+      public string imageURL { get; set; }
+      public int rating { get; set; }
+      public string categoryLogo { get; set; }
+   }
+}
