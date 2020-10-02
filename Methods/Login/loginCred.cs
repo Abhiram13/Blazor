033aaf00f4f06e72e2f7fd6c72a2cfc43026a7ae
Abhiram13 <abhiram131869@gmail.com>
2020-10-02 21:35:07 +0530

    login response has been fetched

--- Methods/Login/loginCred.cs
+++ Methods/Login/loginCred.cs
@@ -2,29 +2,34 @@ using System;
 using System.Net.Http;
 using MongoDB.Driver;
 using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace Authorise {
-     class LoginRequestBody {
-          public string username { get; set; }
+     public class LoginRequestBody {
+          public static string username { get; set; }
      }
 
-     class LoginResponseBody {
+     public class LoginResponseBody {
           public MongoDB.Bson.ObjectId _id { get; set; }
           public string username { get; set; }
           public string firstname { get; set; }
           public string lastname { get; set; }
           public string password { get; set; }
-          public string isAdmin { get; set; }
+          public bool isAdmin { get; set; }
           public int __v { get; set; }
      }
 
      class Login {
-          public static int logUser() {
+          public async Task<LoginResponseBody> logUser() {
                HttpClient client = new HttpClient();
-               string json = JsonSerializer.Serialize(new LoginRequestBody().username);
-               StringContent content = new StringContent(json);
+               string json = JsonSerializer.Serialize(new { username = LoginRequestBody.username });
+               StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
                HttpResponseMessage loginResponse = client.PostAsync("http://localhost:1995/login/", content).Result;
-               return (int) loginResponse.StatusCode;
+               string str = await loginResponse.Content.ReadAsStringAsync();
+               LoginResponseBody res = JsonSerializer.Deserialize<LoginResponseBody>(str);
+               return res;
+               // return (int) loginResponse.StatusCode;
           }
      }
 }
