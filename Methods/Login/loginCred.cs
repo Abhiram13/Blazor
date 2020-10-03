7003da19d990e88601d99ab51bb2768e4545bc38
Abhiram13 <abhiram131869@gmail.com>
2020-10-03 21:44:03 +0530

    login functionality extended to catch wrong creds

--- Methods/Login/loginCred.cs
+++ Methods/Login/loginCred.cs
@@ -8,6 +8,7 @@ using System.Text;
 namespace Authorise {
      public class LoginRequestBody {
           public static string username { get; set; }
+          public static string password { get; set; }
      }
 
      public class LoginResponseBody {
@@ -21,15 +22,21 @@ namespace Authorise {
      }
 
      class Login {
-          public async Task<LoginResponseBody> logUser() {
+          private static HttpResponseMessage responseMessage;
+
+          public static async Task<int> statusCode() {
                HttpClient client = new HttpClient();
-               string json = JsonSerializer.Serialize(new { username = LoginRequestBody.username });
+               string json = JsonSerializer.Serialize(new { username = LoginRequestBody.username, password = LoginRequestBody.password });
                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-               HttpResponseMessage loginResponse = client.PostAsync("http://localhost:1995/login/", content).Result;
-               string str = await loginResponse.Content.ReadAsStringAsync();
+               responseMessage = client.PostAsync("http://localhost:1995/login/", content).Result;
+               Console.WriteLine((int)responseMessage.StatusCode);
+               return (int) responseMessage.StatusCode;
+          }
+          
+          public static async Task<LoginResponseBody> logUser() {
+               string str = await responseMessage.Content.ReadAsStringAsync();
                LoginResponseBody res = JsonSerializer.Deserialize<LoginResponseBody>(str);
                return res;
-               // return (int) loginResponse.StatusCode;
           }
      }
 }
